70d79e980c0025ccb425661cdb134696bc30b42d
hendrix <leandrohendrix@gmail.com>
2022-06-22 09:05:59 -0300

    method sacar

--- Conta.php
+++ Conta.php
@@ -4,5 +4,19 @@ class Conta{
     public $cpf;
     public $titular;
     public $saldo;
+
+  public function sacar(float $valorASacar)
+  {
+    if ($valorASacar > $this->saldo) {
+      echo "Saldo indisponível";
+    } else {
+        $this->saldo -= $valorASacar;
+    }
+  }
 }
+
+$c = new Conta();
+$c->saldo = 200;
+$c->sacar(300);
+echo $c->saldo;
 
