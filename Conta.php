88ffdf997645d51b8d9e204c8cfd855064f75ba5
hendrix <leandrohendrix@gmail.com>
2022-06-23 14:37:19 -0300

    SmallFix

--- Conta.php
+++ Conta.php
@@ -1,22 +1,32 @@
 <?php 
 
-class Conta{
-    public $cpf;
-    public $titular;
-    public $saldo;
+class Conta {
 
-  public function sacar(float $valorASacar)
+  public $cpf;
+  public $titular;
+  public $saldo = 0;
+
+  public function sacar(float $valorASacar): void
   {
     if ($valorASacar > $this->saldo) {
       echo "Saldo indisponível";
     } else {
-        $this->saldo -= $valorASacar;
+      $this->saldo -= $valorASacar;
+    }
+  }
+
+  public function depositar(float $valorADepositar): void
+  {
+    if ($valorADepositar < 0) {
+      echo "Valor precisa ser positivo";
+    } else {
+      $this->saldo += $valorADepositar;
     }
   }
 }
 
 $c = new Conta();
-$c->saldo = 200;
+$c->depositar(900);
 $c->sacar(300);
 echo $c->saldo;
 
